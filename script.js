aefc0f4c3a3db49a09f624b98370d4f88b462b4c
Sisovat <sisovatvanny@gmail.com>
2025-12-11 19:06:39 +0700

    added a button

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+
+
+const button = document.querySelector("button")
+const body = document.querySelector("body")
+
+
+button.addEventListener("click", () => {
+    alert("hello world")
+})
